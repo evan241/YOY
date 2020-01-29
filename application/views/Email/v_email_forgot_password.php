657ec3f2d530de4892c72276f1bcbd82d9615c99
jouo <jashua.heredia@hotmail.com>
2020-01-29 10:25:05 -0600

    squash

--- application/views/Email/v_email_forgot_password.php
+++ application/views/Email/v_email_forgot_password.php
@@ -1,18 +1,30 @@
 <html>
-    <head>
-    <center><img src="<?= base_url(); ?>assets/img/logo_rolling2.png" width="200" class=" img-responsive" alt="izquierda"></center>
+
+<head>
+    <center><img src="<?= base_url(); ?>assets/img/logo_rolling2.png" width="200" class=" img-responsive"
+            alt="izquierda"></center>
 
 </head>
+
 <body>
     <div>
         <div style=" background-color:#00afef; color: white;">
             Hola <?= $ROW_USUARIO[0]['NOMBRE_USUARIO'] ?>
         </div>
         <div>
+
+
+
+
             <br>
+
+
+
+
             tu contraseña es: <?= $ROW_USUARIO[0]['PASSWORD_USUARIO'] ?>
 
         </div>
-    </div> 
+    </div>
 </body>
+
 </html>
